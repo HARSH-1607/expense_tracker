78ba9249ea96317faeb46e0ffeec1fc3631fd360
HARSH-1607 <128159860+HARSH-1607@users.noreply.github.com>
2025-02-10 21:48:33 +0530

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/expense-tracker/',
+  base: '/expense_tracker/',
   optimizeDeps: {
     exclude: ['react-chartjs-2'],
     include: [
